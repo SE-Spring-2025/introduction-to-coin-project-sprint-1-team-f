ddd74fae4d45023979f6aea4441aaadf48cf3f91
josephlaput <156827115+josephlaput@users.noreply.github.com>
2025-03-26 19:48:51 -0400

    Finished Coin.java Class

--- coins-abc-main/src/main/Coin.java
+++ coins-abc-main/src/main/Coin.java
@@ -89,4 +89,63 @@ public class Coin {
 	backLabel = "UNITED STATES OF AMERICA";
 	
     }
+	 private boolean cmpDoubles(double a, double b) {
+	return Math.abs(a-b) < 0.00001;
+    }
+	
+    public String getFamiliarName() {
+	return familiarName;
+    }
+    public double getValue() {
+	return value;
+    }
+    public String getFrontMotto() {
+	return frontMotto;
+    }
+    public String getBackMotto() {
+	return backMotto;
+    }
+    public String getFrontLabel() {
+	return frontLabel;
+    }
+    public String getBackLabel() {
+	return backLabel;
+    }
+    public String getFrontImage() {
+	return frontImage;
+    }
+    public String getBackImage() {
+	return backImage;
+    }
+    public String getValueDescription() {
+	return valueDescription;
+    }
+    public boolean getRidgedEdge() {
+	return ridgedEdge;
+    }
+    public String getMetallurgy() {
+	return metallurgy;
+    }
+    public int getYear() {
+	return manufactureYear;
+    }
+
+    public String toString() {
+	DecimalFormat df = new DecimalFormat("0.00");
+        String formattedValue = df.format(value);
+	
+	return "[" + familiarName
+	    + "," + formattedValue
+	    + "," + manufactureYear
+	    + ",'" + frontMotto
+	    + "','" + backMotto
+	    + "','" + frontImage
+	    + "','" + backImage
+	    + "','" + frontLabel
+	    + "','" + backLabel
+	    + "','" + valueDescription
+	    + "'," + (ridgedEdge ? "ridges" : "smooth")
+	    + ",'" + metallurgy
+	    + "']";
+    }
 }
