59c582135163c39bbe18d9b4969460afc611bec2
rheajd1 <156822210+rheajd1@users.noreply.github.com>
2025-03-26 13:11:24 -0400

    Update Coin.java

--- coins-abc-main/src/main/Coin.java
+++ coins-abc-main/src/main/Coin.java
@@ -27,4 +27,66 @@ public class Coin {
     public Coin(double value) {
 	this(value, (Calendar.getInstance()).get(Calendar.YEAR));
     }
+
+	public Coin(double value, int year) {
+
+	if (cmpDoubles(value, PENNY_VALUE)) {
+	    familiarName = "Penny";
+	    frontImage = "A_Lincoln";
+	    backImage = "Lincoln_Memorial";
+	    valueDescription = "ONE CENT";
+	    ridgedEdge = false;
+	    metallurgy = "Copper";
+	}
+	else if (cmpDoubles(value, NICKEL_VALUE)) {
+	    familiarName = "Nickel";
+	    frontImage = "T_Jefferson";
+	    backImage = "Jefferson_Memorial";
+	    valueDescription = "FIVE CENTS";
+	    ridgedEdge = false;
+	    metallurgy = "Cupro-Nickel";
+	}
+	else if (cmpDoubles(value, DIME_VALUE)) {
+	    familiarName = "Dime";
+	    frontImage = "F_Roosevelt";
+	    backImage = "Torch_Branches";
+	    valueDescription = "ONE DIME";
+	    ridgedEdge = true;
+	    metallurgy = "Cupro-Nickel";
+	}
+	else if (cmpDoubles(value, QUARTER_VALUE)) {
+	    familiarName = "Quarter";
+	    frontImage = "G_Washington";
+	    backImage = "Eagle";
+	    valueDescription = "QUARTER DOLLAR";
+	    ridgedEdge = true;
+	    metallurgy = "Cupro-Nickel";
+	}
+	else if (cmpDoubles(value, HALFDOLLAR_VALUE)) {
+	    familiarName = "HalfDollar";
+	    frontImage = "J_Kennedy";
+	    backImage = "Presidential_Seal";
+	    valueDescription = "HALF DOLLAR";
+	    ridgedEdge = true;
+	    metallurgy = "Cupro-Nickel";
+	}
+	else if (cmpDoubles(value, DOLLAR_VALUE)) {
+	    familiarName = "Dollar";
+	    frontImage = "S_Anthony";
+	    backImage = "Moon_Eagle";
+	    valueDescription = "ONE DOLLAR";
+	    ridgedEdge = true;
+	    metallurgy = "Cupro-Nickel";
+	}
+	else value = 0;
+
+	this.value = value;
+	this.manufactureYear = year;
+
+	frontMotto = "IN GOD WE TRUST";
+	backMotto = "E PLURIBUS UNUM";
+	frontLabel = "LIBERTY";
+	backLabel = "UNITED STATES OF AMERICA";
+	
+    }
 }
